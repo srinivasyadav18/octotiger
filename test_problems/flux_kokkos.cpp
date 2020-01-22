3100337975a2666b36f7bcf6d0a7f083ee4288f6
Theresa Pollinger <pollinta@ipvs.uni-stuttgart.de>
2020-01-22 16:02:22 +0100

    direct comparison: nans returned w/ regularity

--- test_problems/flux_kokkos.cpp
+++ test_problems/flux_kokkos.cpp
@@ -57,26 +57,34 @@ void printState(
 	const safe_real omega,
 	hydro::recon_type<NDIM> q,
 	const safe_real a){
-		for (auto & u : U){
-          printf("%f; ", u);
-		}
-     	printf("\n");
-     	printf("\n");
-		for (auto & u : U0){
-          printf("%f; ", u);
-		}
-     	printf("\n");
-     	printf("\n");
-		for (auto & f : F){
-			for (auto & u : f){
-				printf("%f; ", u);
+		for (auto & u0 : U){
+			for (auto & u : u0){
+				printf("%e; ", u);
 			}
+		}
+     	printf("\n");
+     	printf("\n");
+		for (auto & u0 : U0){
+			for (auto & u : u0){
+				printf("%e; ", u);
+			}
+		}
+     	printf("\n");
+     	printf("\n");
+		for (auto & f0 : F){
+			for (auto & f : f0){
+				for (auto & u : f){
+					printf("%e; ", u);
+				}
+		     	printf("\n");
+			}
+	     	printf("\n");
 		}
      	printf("\n");
      	printf("\n");
 		for (auto & x : X){
 			for (auto & u : x){
-				printf("%f; ", u);
+				printf("%e; ", u);
 			}
 		}
      	printf("\n");
@@ -85,7 +93,7 @@ void printState(
 		for (auto & q0 : q){
 			for (auto & x : q0){
 				for (auto & u : x){
-					printf("%f; ", u);
+					printf("%e; ", u);
 				}
 			}
 		}
@@ -95,17 +103,15 @@ void printState(
 }
 
 
-template<int NDIM, int INX>
+template<int NDIM, int INX, bool with_kokkos>
 void run_test(typename physics<NDIM>::test_type problem, bool with_correction) {
 	static constexpr int H_BW = 3;
 	// static constexpr auto H_NX = INX + 2 * H_BW;
-	static constexpr int H_N3 = std::pow(INX+2*H_BW,NDIM);
+	// static constexpr int H_N3 = std::pow(INX+2*H_BW,NDIM);
+    static constexpr auto H_N3 = static_cast<int>(PowerNDIM(INX+2*H_BW));
 	printf("test H_N3 %d, H_BW %d, NDIM %d, INX %d \n", H_N3, H_BW, NDIM, INX);
-	static_assert(std::pow(206, NDIM) == PowerNDIM(206));
-	static_assert(H_N3 == PowerNDIM(INX+2*H_BW));
-	assert(std::pow(206, NDIM) == PowerNDIM(206));
-	assert(H_N3 == PowerNDIM(INX+2*H_BW));
-
+	// static_assert(std::pow(206, NDIM) == PowerNDIM(206));
+	// static_assert(H_N3 == PowerNDIM(INX+2*H_BW));
 	static constexpr safe_real CFL = (0.4 / NDIM);
 	hydro_computer<NDIM, INX> computer;
 	if (with_correction) {
@@ -136,19 +142,35 @@ void run_test(typename physics<NDIM>::test_type problem, bool with_correction) {
 		U0 = U;
 		auto q = computer.reconstruct(U, X, omega);
 		first_q = q;
-		auto a = computer.flux(U, q, F, X, omega);
+		safe_real a;
+		if (with_kokkos){
+	        a = octotiger::compute_flux_kokkos<NDIM, INX>(computer, U0, U, q, F, X, omega, nf, H_N3);
+		}
+		else{
+			a = computer.flux(U, q, F, X, omega);
+		}
+		printState<NDIM>(U, U0, F, X, omega, q, a);
 		safe_real dt = CFL * dx / a;
 		dt = std::min(double(dt), tmax - t + 1.0e-20);
 		computer.advance(U0, U, F, X, dx, dt, 1.0, omega);
 		computer.boundaries(U);
 		q = computer.reconstruct(U, X, omega);
-		computer.flux(U, q, F, X, omega);
-		printState<NDIM>(U, U0, F, X, omega, q, a);
+		if (with_kokkos){
+	        octotiger::compute_flux_kokkos<NDIM, INX>(computer, U0, U, q, F, X, omega, nf, H_N3);
+		}
+		else{
+			computer.flux(U, q, F, X, omega);
+		}
 		computer.advance(U0, U, F, X, dx, dt, 0.25, omega);
 		computer.boundaries(U);
 		// printState<NDIM>(U, U0, F, X, omega, q, a);
 		q = computer.reconstruct(U, X, omega);
-		computer.flux(U, q, F, X, omega);
+		if (with_kokkos){
+	        octotiger::compute_flux_kokkos<NDIM, INX>(computer, U0, U, q, F, X, omega, nf, H_N3);
+		}
+		else{
+			computer.flux(U, q, F, X, omega);
+		}
 		computer.advance(U0, U, F, X, dx, dt, 2.0 / 3.0, omega);
 		computer.boundaries(U);
 		computer.post_process(U, dx);
@@ -201,6 +223,73 @@ void run_test(typename physics<NDIM>::test_type problem, bool with_correction) {
 	FILE* fp = fopen( "time.dat", "wt");
 	fprintf( fp, "%i %li\n", INX, tstop -tstart);
 	fclose(fp);
+}
+
+
+template<int NDIM, int INX>
+void compare(typename physics<NDIM>::test_type problem, bool with_correction) {
+	static constexpr int H_BW = 3;
+	// static constexpr auto H_NX = INX + 2 * H_BW;
+	// static constexpr int H_N3 = std::pow(INX+2*H_BW,NDIM);
+    static constexpr auto H_N3 = static_cast<int>(PowerNDIM(INX+2*H_BW));
+	printf("test H_N3 %d, H_BW %d, NDIM %d, INX %d \n", H_N3, H_BW, NDIM, INX);
+	// static_assert(std::pow(206, NDIM) == PowerNDIM(206));
+	// static_assert(H_N3 == PowerNDIM(INX+2*H_BW));
+	// assert(std::pow(206, NDIM) == PowerNDIM(206));
+	// assert(H_N3 == PowerNDIM(INX+2*H_BW));
+
+	hydro_computer<NDIM, INX> computer;
+	if (with_correction) {
+		computer.use_angmom_correction(physics<NDIM>::variables_.sx_i, 1);
+	}
+	const auto nf = physics<NDIM>::field_count();
+	std::vector<std::vector<std::vector<safe_real>>> F_ref(NDIM, std::vector<std::vector<safe_real>>(nf, std::vector<safe_real>(H_N3)));
+	auto F = F_ref;
+	std::vector<std::vector<safe_real>> U(nf, std::vector<safe_real>(H_N3));
+	std::vector<std::vector<safe_real>> U0(nf, std::vector<safe_real>(H_N3));
+	hydro::x_type X(NDIM);
+	for (int dim = 0; dim < NDIM; dim++) {
+		X[dim].resize(H_N3);
+	}
+
+	int oter = 0;
+	physics<NDIM> phys;
+	computer.set_bc(phys.template initialize<INX>(problem, U, X));
+	const safe_real dx = X[0][cell_geometry<NDIM, INX>::H_DNX] - X[0][0];
+	computer.output(U, X, oter++, 0);
+//	const safe_real omega = 2.0 * M_PI / tmax / 10.0;
+	const safe_real omega = 0.0;
+	printf("omega = %e\n", (double) omega);
+
+	U0 = U;
+	auto q = computer.reconstruct(U, X, omega);
+	first_q = q;
+	safe_real a_ref, a;
+	a = octotiger::compute_flux_kokkos<NDIM, INX>(computer, U0, U, q, F, X, omega, nf, H_N3);
+	a_ref = computer.flux(U, q, F_ref, X, omega);
+	printf("a: %e %e \n", a, a_ref);
+
+	// printState<NDIM>(U, U0, F, X, omega, q, a);
+	for (int i=0; i < F.size(); ++i){
+		for (int j=0; j < F[i].size(); ++j){
+			for (int k=0; k < F[i][j].size(); ++k){
+				auto diff = abs(F[i][j][k] - F_ref[i][j][k]);
+				bool is_same = ((diff/F[i][j][k] < 1e-5) && (diff/F_ref[i][j][k] < 1e-5)) || diff < 1e-15;
+				if (!is_same){
+					printf("%d %d %d: %e %e; ", i, j, k, F[i][j][k], diff);
+				}
+			}
+		}
+    	printf("\n");
+	}
+
+	// static constexpr safe_real CFL = (0.4 / NDIM);
+	// safe_real t = 0.;
+	// safe_real dt = CFL * dx / a;
+	// dt = std::min(double(dt), tmax - t + 1.0e-20);
+	// computer.advance(U0, U, F, X, dx, dt, 1.0, omega);
+	// computer.boundaries(U);
+	// q = computer.reconstruct(U, X, omega);
 }
 
 // for timing purposes, test with random numbers
@@ -263,7 +352,7 @@ void test_random_numbers() {
 		for (int i=0; i < NDIM; ++i){
 			for (int j=0; j < nf; ++j){
 				for (int k=0; k < H_N3; ++k){
-					if( std::abs( F[i][j][k] - F_comp[i][j][k] ) > 1e-18 ){
+					if( std::abs( F[i][j][k] - F_comp[i][j][k] ) > 1e-14 ){
 						printf("F %.5e %.5e; ", F[i][j][k], F[i][j][k] - F_comp[i][j][k]);
 					}
 				}
@@ -272,137 +361,13 @@ void test_random_numbers() {
 		printf("\n");
 		for (int i=0; i < NDIM; ++i){
 			for (int k=0; k < H_N3; ++k){
-				if( std::abs( X[i][k] - X_comp[i][k] ) > 1e-18 ){
+				if( std::abs( X[i][k] - X_comp[i][k] ) > 1e-14 ){
 					printf("X %f %f; ", X[i][k], X_comp[i][k]);
 				}
 			}
 		}	
 #endif 
 	}
-}
-
-template <int NDIM, int INX>
-void run_test_kokkos(typename physics<NDIM>::test_type problem, bool with_correction) {
-	static constexpr auto H_BW = 3;
-	// static constexpr auto H_NX = INX + 2 * H_BW;
-    // static constexpr auto H_N3 = static_cast<int>(std::pow(INX + 2 * H_BW, NDIM));
-    static constexpr auto H_N3 = static_cast<int>(PowerNDIM(INX+2*H_BW));
-	printf("test H_N3 %d, H_BW %d, NDIM %d, INX %d \n", H_N3, H_BW, NDIM, INX);
-
-	static constexpr safe_real CFL = (0.4 / NDIM);
-	hydro_computer<NDIM, INX> computer;
-	if (with_correction) {
-		computer.use_angmom_correction(physics<NDIM>::variables_.sx_i, 1);
-	}
-	const auto nf = physics<NDIM>::field_count();
-	// assert all our indices are integral
-	static_assert(std::is_integral<decltype(NDIM)>::value);
-	static_assert(std::is_integral<decltype(nf)>::value);
-	static_assert(std::is_integral<decltype(H_N3)>::value);
-	
-    std::vector<std::vector<std::vector<safe_real>>> F(
-        NDIM, std::vector<std::vector<safe_real>>(nf, std::vector<safe_real>(H_N3)));
-	std::vector<std::vector<safe_real>> U(nf, std::vector<safe_real>(H_N3));
-	std::vector<std::vector<safe_real>> U0(nf, std::vector<safe_real>(H_N3));
-
-	// x_type = std::vector<std::vector<safe_real>>;
-	hydro::x_type X(NDIM);
-	for (int dim = 0; dim < NDIM; dim++) {
-		X[dim].resize(H_N3);
-	}
-
-	safe_real t = 0.0;
-	int iter = 0;
-	int oter = 0;
-	physics<NDIM> phys;
-	computer.set_bc(phys.template initialize<INX>(problem, U, X));
-	const safe_real dx = X[0][cell_geometry<NDIM, INX>::H_DNX] - X[0][0];
-	computer.output(U, X, oter++, 0);
-//	const safe_real omega = 2.0 * M_PI / tmax / 10.0;
-	const safe_real omega = 0.0;
-	printf("omega = %e\n", (double) omega);
-
-	const auto tstart = time(NULL);
-	while (t < tmax) {
-		U0 = U;
-		// this is the return type of q
-		// recon_type =std::vector<std::vector<std::array<safe_real, NDIM == 1 ? 3 : (NDIM == 2 ? 9 : 27)>>>;
-		auto q = computer.reconstruct(U, X, omega);
-		if (first_q == q){
-			printf("EQUAL!");
-		}
-		auto a = computer.flux(U, q, F, X, omega);
-		safe_real dt = CFL * dx / a;
-		dt = std::min(double(dt), tmax - t + 1.0e-20);
-		computer.advance(U0, U, F, X, dx, dt, 1.0, omega);
-		computer.boundaries(U);
-		q = computer.reconstruct(U, X, omega);
-        octotiger::compute_flux_kokkos<NDIM, INX>(computer, U0, U, q, F, X, omega, nf, H_N3);
-		printState<NDIM>(U, U0, F, X, omega, q, a);
-		// for (int i; i < q.size(); ++i){
-		// 	for (int j; j < q[i].size(); ++j){
-		// 		for (int k; k < q[i][j].size(); ++k){
-					// if ()
-		// 		}
-		// 	}
-		// }
-		computer.advance(U0, U, F, X, dx, dt, 0.25, omega);
-		computer.boundaries(U);
-		// printState<NDIM>(U, U0, F, X, omega, q, a);
-		q = computer.reconstruct(U, X, omega); //NO SPECIES!
-		computer.flux(U, q, F, X, omega);
-		computer.advance(U0, U, F, X, dx, dt, 2.0 / 3.0, omega);
-		computer.boundaries(U);
-		computer.post_process(U, dx);
-		t += dt;
-		computer.boundaries(U);
-		if (int(t / dt_out) != int((t - dt) / dt_out))
-			computer.output(U, X, oter++, t);
-		iter++;
-		printf("%i %e %e\n", iter, double(t), double(dt));
-	}
-	const auto tstop = time(NULL);
-	U0 = U;
-	physics<NDIM>::template analytic_solution<INX>(problem, U, X, t);
-	computer.output(U, X, iter++, t);
-
-	phys.template pre_recon<INX>(U0, X, omega, with_correction);
-	phys.template pre_recon<INX>(U, X, omega, with_correction);
-	std::vector<safe_real> L1(nf);
-	std::vector<safe_real> L2(nf);
-	std::vector<safe_real> Linf(nf);
-	for (int f = 0; f < nf; f++) {
-		L1[f] = L2[f] = Linf[f];
-		for (int i = 0; i < H_N3; i++) {
-			L1[f] += std::abs(U0[f][i] - U[f][i]);
-			L2[f] += std::pow(U0[f][i] - U[f][i], 2);
-			Linf[f] = max_device((double) Linf[f], std::abs(U0[f][i] - U[f][i]));
-		}
-		L2[f] = sqrt(L2[f]);
-		L1[f] /= INX * INX;
-		L2[f] /= INX * INX;
-	}
-
-	FILE *fp1 = fopen("L1_k.dat", "at");
-	FILE *fp2 = fopen("L2_k.dat", "at");
-	FILE *fpinf = fopen("Linf_k.dat", "at");
-	fprintf(fp1, "%i ", INX);
-	fprintf(fp2, "%i ", INX);
-	fprintf(fpinf, "%i ", INX);
-	for (int f = 0; f < nf; f++) {
-		fprintf(fp1, "%e ", (double) L1[f]);
-		fprintf(fp2, "%e ", (double) L2[f]);
-		fprintf(fpinf, "%e ", (double) Linf[f]);
-	}
-	fprintf(fp1, "\n");
-	fprintf(fp2, "\n");
-	fprintf(fpinf, "\n");
-	fclose(fp1);
-	fclose(fp2);
-	fclose(fpinf);
-	FILE* fp = fopen( "time.dat", "wt");
-	fprintf( fp, "%i %li\n", INX, tstop -tstart);
-	fclose(fp);
 }
 
 int main(int argc, char** argv) {
@@ -414,10 +379,9 @@ int main(int argc, char** argv) {
 	feenableexcept(FE_INVALID);
 	feenableexcept(FE_OVERFLOW);
 
-	test_random_numbers<2, 10>();
+	// test_random_numbers<2, 10>();
 
 #if !defined(__CUDA_ARCH__)
-	run_test<2, 8>(physics<2>::BLAST, true);
 	// run_test<2, 200>(physics<2>::BLAST, true);
 	// // 1 1.384974e+03 7.220354e-07 7.220354e-07
 	// // 2 7.828761e+02 1.999377e-06 1.277341e-06
@@ -443,13 +407,19 @@ int main(int argc, char** argv) {
 	//  or add -Ihpx.stacks.use_guard_pages=0 to the command line: HPX(unhandled_exception)`
 	// test_random_numbers<3, 50>();
 	// test_random_numbers<2, 200>();
-	// run_test_kokkos<3, 50>(physics<3>::BLAST, true);
-	run_test_kokkos<2, 10>(physics<2>::BLAST, true);
+	// run_tests<3, 50, true>(physics<3>::BLAST, true);
+	// run_test<2, 8, false>(physics<2>::BLAST, true);
+	// run_test<2, 8, true>(physics<2>::BLAST, true); // identical
+	// compare<2, 8>(physics<2>::BLAST, true);
+	compare<2, 10>(physics<2>::BLAST, true);
+	// compare<2, 20>(physics<2>::BLAST, true);
+	// compare<2, 100>(physics<2>::BLAST, true);
+	// compare<2, 200>(physics<2>::BLAST, true);
+	// compare<2, 200>(physics<2>::BLAST, true);
+	// run_test<2, 10, false>(physics<2>::BLAST, true);
+	// run_test<2, 10, true>(physics<2>::BLAST, true);
 	// flux_kokkos: 0.00442272 seconds
-
-
-
-
+	
     Kokkos::finalize();
 	return 0;
 }
