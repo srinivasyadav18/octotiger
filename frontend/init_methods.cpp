faeaa6527b49c9d66d7e06056dd530e8a3e29202
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-09-01 16:40:58 +0200

    Fixed hip ifdef

--- frontend/init_methods.cpp
+++ frontend/init_methods.cpp
@@ -130,11 +130,14 @@ void init_executors(void) {
 #if defined(KOKKOS_ENABLE_CUDA)
     // initialize stencils / executor pool in kokkos device
     std::cout << "KOKKOS/CUDA is enabled!" << std::endl;
+    stream_pool::init<hpx::kokkos::cuda_executor, round_robin_pool<hpx::kokkos::cuda_executor>>(
+        opts().cuda_streams_per_gpu, hpx::kokkos::execution_space_mode::independent);
 #elif defined(KOKKOS_ENABLE_HIP)
     std::cout << "KOKKOS/HIP is enabled!" << std::endl;
-#endif
     stream_pool::init<hpx::kokkos::hip_executor, round_robin_pool<hpx::kokkos::hip_executor>>(
         opts().cuda_streams_per_gpu, hpx::kokkos::execution_space_mode::independent);
+#endif
+#if defined(KOKKOS_ENABLE_CUDA) || defined(KOKKOS_ENABLE_HIP)
     kokkos_device_executor mover{};
     octotiger::fmm::monopole_interactions::get_device_masks<device_buffer<int>, host_buffer<int>,
         kokkos_device_executor>(mover);
@@ -149,6 +152,7 @@ void init_executors(void) {
     std::cout << "KOKKOS with polling futures enabled!" << std::endl;
 #else
     std::cout << "KOKKOS with callback futures enabled!" << std::endl;
+#endif
 #endif
 #endif
 
